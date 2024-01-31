534acc4c381c42cb2a95b1ba961bf932e7b264c2
Clint Animbom <clint360.rebase@gmail.com>
2024-01-31 12:23:08 +0100

    Redefinde Exports

--- src/constants/index.js
+++ src/constants/index.js
@@ -47,86 +47,33 @@ export const navLinks = [
 ];
 
 const services = [
-  {
-    title: "Full Stack Developer",
-    icon: web,
-  },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
-  {
-    title: "Technical Writer",
-    icon: backend,
-  },
-  {
-    title: "Content Creator",
-    icon: creator,
-  },
-  {
-    title: "Graphics Designer",
-    icon: web,
-  },
-  {
-    title: "Sound Designer",
-    icon: mobile,
-  },
+  // {
+  //   title: "Full Stack Developer",
+  //   icon: web,
+  // },
+  // {
+  //   title: "React Native Developer",
+  //   icon: mobile,
+  // },
+  // {
+  //   title: "Technical Writer",
+  //   icon: backend,
+  // },
+  // {
+  //   title: "Content Creator",
+  //   icon: creator,
+  // },
+  // {
+  //   title: "Graphics Designer",
+  //   icon: web,
+  // },
+  // {
+  //   title: "Sound Designer",
+  //   icon: mobile,
+  // },
 ];
 
 const technologies = []
-//   {
-//     name: "HTML 5",
-//     icon: html,
-//   },
-//   {
-//     name: "CSS 3",
-//     icon: css,
-//   },
-//   {
-//     name: "JavaScript",
-//     icon: javascript,
-//   },
-//   {
-//     name: "TypeScript",
-//     icon: typescript,
-//   },
-//   {
-//     name: "React JS",
-//     icon: reactjs,
-//   },
-//   {
-//     name: "Redux Toolkit",
-//     icon: redux,
-//   },
-//   {
-//     name: "Tailwind CSS",
-//     icon: tailwind,
-//   },
-//   {
-//     name: "Node JS",
-//     icon: nodejs,
-//   },
-//   {
-//     name: "MongoDB",
-//     icon: mongodb,
-//   },
-//   {
-//     name: "Three JS",
-//     icon: threejs,
-//   },
-//   {
-//     name: "git",
-//     icon: git,
-//   },
-//   {
-//     name: "figma",
-//     icon: figma,
-//   },
-//   {
-//     name: "docker",
-//     icon: docker,
-//   },
-// ];
 
 const experiences = [
   {
